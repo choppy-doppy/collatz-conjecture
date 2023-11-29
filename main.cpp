0e66cf379c5b57f4d5d964cd6d94ef167ffbf1c9
Choppy <kaedongeddes@sd28.org>
2023-11-29 09:43:55 -0800

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main () {
 
-    cout << "yippie" << endl;
+    int n = 1;
 
+    cout << "original value: " << n << endl;
+
+    for ( ; ; ) {
+
+        if (n % 2 == 0) {
+            n / 2;
+        } else {
+            n * 2 + 1;
+        }
+
+        cout << "current value: " << n << endl;
+
+        if (n == 1)
+            n++;
+
+        cout << "n = " << n << endl;
+    }
 }
